a22e6b60129d9bf95a68ec3183adc6421597de7e
joel langat <langatjoel42@gmail.com>
2024-04-15 11:08:49 +0300

    added an event listener

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ document.getElementById('fetchCocktailButton').addEventListener('click' , fetchC
 
 // Event listener for searching for a cocktail.
 document.getElementById('searchInput').addEventListener('input' , searchCocktail);
+
+// Event listener for searching for a cocktail when Enter key is pressed.
+document.getElementById('searchInput').addEventListener('keypress', function(event))
  
 // Function to fetch a random cocktail.
 function fetchCocktail(){
